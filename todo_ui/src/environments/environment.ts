18b74e5fe29e9ec08d01395b2bee80e9dee81fd1
vdhruvannaidu <vdhruvannaidu@gmail.com>
2024-09-23 01:49:16 +0530

    commit

---
+++ todo_ui/src/environments/environment.ts
@@ -0,0 +1,12 @@
+export const environment = {
+    production: false,
+    firebase: {
+        apiKey: "AIzaSyDpsnbNi15MnuqAwFc0fR2h5gEArTsIF_A",
+        authDomain: "ngtodoist.firebaseapp.com",
+        projectId: "ngtodoist",
+        storageBucket: "ngtodoist.appspot.com",
+        messagingSenderId: "727288303659",
+        appId: "1:727288303659:web:84d664dee0d8afa5ef5a6e",
+        measurementId: "G-3ECRFVJ9KQ"
+    }
+  };
